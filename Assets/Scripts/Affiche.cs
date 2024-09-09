a016de8d46b4adc8347c8244458dda1627e84999
Francois Lahalle <flahalle28@gmail.com>
2024-09-10 00:04:48 +0200

    NEW : Affiche Suivante + UI

--- Assets/Scripts/Affiche.cs
+++ Assets/Scripts/Affiche.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Overlays;
 using UnityEngine;
 
 public class Affiche : MonoBehaviour
 {
+    [SerializeField] private Vector2 stockPosition = new Vector2(1000, 0);
+    bool isNext = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,16 @@ public class Affiche : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isNext)
+        {
+            Debug.Log(transform.position);
+            transform.position = Vector2.Lerp(transform.position, stockPosition, 5.0f * Time.deltaTime);
+        }
+    }
+
+    public void Next()
+    {
+        Debug.Log("Next");
+        isNext = true;
     }
 }
--- Assets/Scripts/Stack.cs
+++ Assets/Scripts/Stack.cs
@@ -1,15 +1,18 @@
 using Nova.TMP;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Stacks : MonoBehaviour
 {
     private GameManager gameManager;
     [SerializeField] private int indexActualAffiche = 0;
-    [SerializeField] private Affiche actualAffiche;
-    private List<Affiche> affichesUndone = new List<Affiche>(10);
-    private List<Affiche> affichesDone = new List<Affiche>(10);
+    [SerializeField] private GameObject prefabAffiche;
+
+    [SerializeField] private GameObject actualAffiche;
+    private List<GameObject> affichesUndone = new List<GameObject>(10);
+    private List<GameObject> affichesDone = new List<GameObject>(10);
     [SerializeField] private TextMeshProTextBlock remainingAffiche;
 
     // Start is called before the first frame update
@@ -22,7 +25,9 @@ public class Stacks : MonoBehaviour
 
         for (int i = 0; i < 10; i++)
         {
-            affichesUndone.Add(new Affiche());
+            GameObject instance = Instantiate(prefabAffiche, transform.position, Quaternion.identity);
+
+            affichesUndone.Add(instance);
         }
         actualAffiche = affichesUndone[0];
         ChangeRemainingAfficheText();
@@ -31,7 +36,7 @@ public class Stacks : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+   
     }
 
     public void ChangeRemainingAfficheText()
@@ -42,12 +47,16 @@ public class Stacks : MonoBehaviour
 
     public void NextAffiche()
     {
+        Debug.Log("Next First");
+        actualAffiche.GetComponent<Affiche>().Next();
         affichesDone.Add(actualAffiche);
         affichesUndone.Remove(actualAffiche);
                ChangeRemainingAfficheText();
         if (affichesUndone.Count > 0) actualAffiche = affichesUndone[0];
         else gameManager.PauseGame();
     }
+
+ 
 
 
 }
