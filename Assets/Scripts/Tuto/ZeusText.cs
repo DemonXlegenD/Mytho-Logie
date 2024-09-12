a094d0cbbe183290f0283bbe9b7ebee69e722217
PIRIS Marius <marius.piris@gmail.com>
2024-09-12 22:09:55 +0200

    Crash wtf ?

--- Assets/Scripts/Tuto/ZeusText.cs
+++ Assets/Scripts/Tuto/ZeusText.cs
@@ -2,7 +2,7 @@ using Nova.TMP;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.WSA;
+//using UnityEngine.WSA;
 
 public class ZeusText : MonoBehaviour
 {
@@ -16,22 +16,22 @@ public class ZeusText : MonoBehaviour
     private void Start()
     {
         textList.Add("Bonjour esclave, c'est l'heure de travailler!");
-        textList.Add("J'espère que tu es prêt car c'est une tâche très difficile.");
-        textList.Add("Tellement difficile qu'un misérable humain comme toi ne pourra y parvenir.");
-        textList.Add("Un peu comme ma première femme.");
+        textList.Add("J'espï¿½re que tu es prï¿½t car c'est une tï¿½che trï¿½s difficile.");
+        textList.Add("Tellement difficile qu'un misï¿½rable humain comme toi ne pourra y parvenir.");
+        textList.Add("Un peu comme ma premiï¿½re femme.");
         textList.Add("ENFIN BREF.");
-        textList.Add("Ton travail est de créer des affiches de propagande pour nos clients.");
+        textList.Add("Ton travail est de crï¿½er des affiches de propagande pour nos clients.");
         textList.Add("Ici tu as les affiches.");
         textList.Add("Tu devras y coller des stickers.");
-        textList.Add("Sais-tu où sont les stickers?.");
-        textList.Add("Incompétant !!!");
-        textList.Add("Ils sont là!");
+        textList.Add("Sais-tu oï¿½ sont les stickers?.");
+        textList.Add("Incompï¿½tant !!!");
+        textList.Add("Ils sont lï¿½!");
         textList.Add("Bien maintenant tu peux prendre un stickers.");
-        textList.Add("Maintenant tu le places sur l'affiche à l'endroit exact qui est pointé!");
-        textList.Add("Tu l'as mal collé! Que vais-je faire de toi");
-        textList.Add("Bon laisse tomber. Si tu penses que ton affiche est bonne, alors tu peux passer à la suivante en cliquant ici!");
+        textList.Add("Maintenant tu le places sur l'affiche ï¿½ l'endroit exact qui est pointï¿½!");
+        textList.Add("Tu l'as mal collï¿½! Que vais-je faire de toi");
+        textList.Add("Bon laisse tomber. Si tu penses que ton affiche est bonne, alors tu peux passer ï¿½ la suivante en cliquant ici!");
         textList.Add("Bravo tu as tout compris. Je savais que je pouvais compter sur toi");
-        textList.Add("Travaille bien, nos clients sont très exigents, et surtout pas de bêtises, sinon on va encore croire que c'est moi...");
+        textList.Add("Travaille bien, nos clients sont trï¿½s exigents, et surtout pas de bï¿½tises, sinon on va encore croire que c'est moi...");
         ChangeText(textList[tutoIndex]);
     }
 
