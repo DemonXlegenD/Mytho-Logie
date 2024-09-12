3ce6a797ce25fe8e03ace5b85f6d21a973e0da7f
Hugo Le Bolloc'h <hugolb.pro@gmail.com>
2024-09-12 12:46:05 +0200

    [ADD] Sticker SFX in unity

--- Assets/Scripts/Stickers/DragAndDrop.cs
+++ Assets/Scripts/Stickers/DragAndDrop.cs
@@ -107,6 +107,7 @@ public class DragAndDrop : MonoBehaviour
 
     void OnMouseDown()
     {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Sticker_pickup");
         Cursor.SetCursor(cursorTextureClic, hotSpot, cursorMode);
         offset = transform.position - GetMouseWorldPos();
         spriteRenderer.sortingOrder = sortingOrder++;
@@ -132,6 +133,7 @@ public class DragAndDrop : MonoBehaviour
 
     void OnMouseUp()
     {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Sticker_stamp");
         isDragging = false;
         Cursor.SetCursor(cursorTextureHover, Vector2.zero, cursorMode);
         if (isTextSticker)
